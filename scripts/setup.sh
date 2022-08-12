241e232487fac9bae680f645f759d618578451a8
royrustdev <royrustdev@gmail.com>
2022-08-12 11:21:11 +0530

    fix typo

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,14 +6,14 @@ sudo dnf insatll fish
 echo "installing rust..."
 # install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-echo "rust instalation complete..."
+echo "rust installation complete..."
 
 echo "installing fnm..."
 # install fnm for nodejs install
 curl -fsSL https://fnm.vercel.app/install | bash
-echo "fnm instalation complete..."
+echo "fnm installation complete..."
 
 echo "installing pnpm..."
 # install pnpm
 curl -fsSL https://get.pnpm.io/install.sh | sh -
-echo "pnpm instalation complete..."
+echo "pnpm installation complete..."
