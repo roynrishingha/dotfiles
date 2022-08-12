9c4640715946ebb28106a61a910bbc6806d281d0
royrustdev <royrustdev@gmail.com>
2022-08-12 10:09:44 +0530

    add fish config abbr

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,23 @@
-set PATH $HOME/.cargo/bin $PATH
+abbr -a c cargo
+abbr -a e nvim
+abbr -a m make
+abbr -a g git
+abbr -a gc 'git commit'
+abbr -a gs 'git status'
+abbr -a gp 'git push'
+abbr -a ga 'git add -p'
+
+if command -v exa > /dev/null
+	abbr -a l 'exa'
+	abbr -a ls 'exa'
+	abbr -a ll 'exa -l'
+	abbr -a lll 'exa -la'
+else
+	abbr -a l 'ls'
+	abbr -a ll 'ls -l'
+	abbr -a lll 'ls -la'
+end
+
+
 zoxide init fish | source
 starship init fish | source
-
